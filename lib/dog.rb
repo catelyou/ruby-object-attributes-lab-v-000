79f113be4f7cbb883a68a4a9e375d28b37185dbb
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-13 16:12:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 class Dog
 
-  def name= @name
-    name = @name
+  def name= (dogs_name)
+    @name = dogs_name
   end
 
   def name
     @name
   end
 
-  def breed= @breed
-    breed = @breed
+  def breed= (dogs_breed)
+    @breed = dogs_breed 
   end
 
   def breed
